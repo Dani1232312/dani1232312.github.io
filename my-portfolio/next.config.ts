b478dfc6bee9e83d1656827c817d961b3d78f01a
Daniel Botnarenco <danielbotnarenco@Daniels-MacBook-Pro.local>
2025-10-13 20:31:38 +0300

    added workflow

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/my-portfolio', // replace if using a repo like github.io/my-portfolio
 };
 
 export default nextConfig;
